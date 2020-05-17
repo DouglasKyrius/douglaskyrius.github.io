34ee9347fcffceede6539a5f662dafc1a49a43c1
DouglasKyrius <douglaskyrius@gmail.com>
2020-05-16 22:46:02 -0400

    uploading robotfriends

---
+++ precache-manifest.6e0943835677f748c2d0c379ce69c24e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd7de6d4e197e9ee3ee31c1433a6987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b2d0ae3c581e96de4db",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2169b1d4042df9af80bf",
+    "url": "/static/css/main.458a9a62.chunk.css"
+  },
+  {
+    "revision": "4b2d0ae3c581e96de4db",
+    "url": "/static/js/2.27a26a1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27a26a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2169b1d4042df9af80bf",
+    "url": "/static/js/main.bb5d4c69.chunk.js"
+  },
+  {
+    "revision": "31b6202e5740a78af56c",
+    "url": "/static/js/runtime-main.3ac41dd5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
